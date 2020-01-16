776b74b515f6b35a8ea64c8988b43836817a91fd
Thomas Desrosiers <git@hive.pw>
2020-01-15 18:12:22 -0800

    Fix: shadow's table schema

--- src/db/schemas/shadow.sql
+++ src/db/schemas/shadow.sql
@@ -1,13 +1,16 @@
-CREATE TABLE "shadow" IF NOT EXISTS (
+-- Types based on `glibc/shadow/shadow.h`
+-- Nullability in this type was defined based on `glibc/shadow/sgetspent_r.c`
+
+CREATE TABLE IF NOT EXISTS "shadow"  (
     "username"                      TEXT    NOT NULL,
     "encrypted_password"            TEXT    NOT NULL DEFAULT "*",
-    "date_of_last_password_change"  INTEGER NOT NULL,
-    "minimum_password_age"          INTEGER NOT NULL,
-    "maximum_password_age"          INTEGER NOT NULL,
-    "password_warning_period"       INTEGER NOT NULL,
-    "password_inactivity_period"    INTEGER NOT NULL,
-    "account_expiration_date"       INTEGER NOT NULL,
-    "reserved_field"                BLOB    NOT NULL
+    "date_of_last_password_change"  INTEGER,
+    "minimum_password_age"          INTEGER,
+    "maximum_password_age"          INTEGER,
+    "password_warning_period"       INTEGER,
+    "password_inactivity_period"    INTEGER,
+    "account_expiration_date"       INTEGER,
+    "reserved_field"                INTEGER -- Should be an u32, but that'll fit in i64
 );
 
 CREATE INDEX IF NOT EXISTS  "shadow_username"  ON shadow(username);
